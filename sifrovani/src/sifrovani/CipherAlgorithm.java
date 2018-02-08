400cb5b63b8b5b2edfc27bfaa2d672d265ed1fba
Student <Student@DESKTOP-PTKTPAI>
2018-02-08 19:10:54 +0100

    abstraktní třída CipherAlgorithm s abstratkními metodami a implementovaným rozhraním Cipher

---
+++ sifrovani/src/sifrovani/CipherAlgorithm.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sifrovani;
+
+import java.io.File;
+
+/**
+ *
+ * @author Student
+ */
+public abstract class CipherAlgorithm implements Cipher {
+
+    protected String texttocipher;
+
+    public CipherAlgorithm() {
+    }
+
+    public CipherAlgorithm(String texttocipher) {
+        this.texttocipher = texttocipher;
+
+    }
+public abstract void prepareText();
+
+//public abstract void cipherText();
+
+//public abstract void decipherText();
+
+    @Override
+    public abstract void cipher(String string);
+
+    @Override
+    public abstract void cipher(File file);
+
+    @Override
+    public abstract void decipher(String string); 
+
+    @Override
+    public abstract void decipher(File file); 
+}
