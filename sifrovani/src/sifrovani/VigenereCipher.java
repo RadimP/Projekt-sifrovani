e5e11d130f12d3edb3947ad4850e1f207a4628df
Student <Student@DESKTOP-PTKTPAI>
2018-02-15 20:47:21 +0100

    upraveno pro novou metodu úpravy stringu z helperu

--- sifrovani/src/sifrovani/VigenereCipher.java
+++ sifrovani/src/sifrovani/VigenereCipher.java
@@ -27,75 +27,20 @@ import java.util.logging.Logger;
 public class VigenereCipher extends CipherAlgorithm implements Serializable {
 
     // http://javarevisited.blogspot.cz/2012/12/how-to-read-input-from-command-line-in-java.html interaktivní zadávání vstupu z commandline   
-    private String key ="";
+    private String key = "";
     private String decipheredtext;
 
     public VigenereCipher() {
     }
 
     public VigenereCipher(String texttocipher) {
-        /* if(texttocipher.length()<3) { 
-         throw new IllegalArgumentException("Text, který se má šifrovat, je kratší než tři znaky");} else
-         {  this.texttocipher = texttocipher;
-         if(this.removeAllNonletterCharacters().length()<3) { 
-         throw new IllegalArgumentException("Text, který se má šifrovat, bude po úpravě kratší než tři znaky");}
-         }*/
-        this.texttocipher = texttocipher;
+                this.texttocipher = texttocipher;
     }
 
-    /**
-     * Tato metoda zbaví text všech znaků, jež nejsou písmena, ořeže diakritiku
-     * a převede text na velká písmena.
-     *
-     * @return vrátí očištěný String texttocipher
-     */
-    @Override
-    public String prepareText() {
-        this.removeAllNonletterCharacters();
-        this.replaceDiacritics();
-        this.textToUpperCases();
-        return this.texttocipher;
-    }
+    
+    
 
-    /**
-     * Tato metoda zbaví text všech znaků, jež nejsou písmena, a odstraní
-     * mezery.
-     *
-     * @return vrátí String texttocipher, v němž zůstanou jen písmena (tedy bez
-     * mezer, čísel, interpunkce a jiných nepísmenných znaků
-     */
-    public String removeAllNonletterCharacters() {
-        this.texttocipher = this.texttocipher.replaceAll("[^\\p{L} ]", "").replaceAll("\\s", "");
-        return this.texttocipher;
-    }
-
-    /**
-     * Tato metoda odstraní ze znaků diakritiku.
-     *
-     * @return vrátí String texttocipher bez diakritiky
-     */
-    public String replaceDiacritics() {
-        /*  String[] diacritics = new String[]{"À", "Á", "Â", "Ã", "Ä", "Å", "Ç", "È", "É", "Ê", "Ë", "Ì", "Í", "Î", "Ï", "Ñ", "Ò", "Ó", "Ô", "Õ", "Ö", "Ù", "Ú", "Û", "Ü", "Ý", "ß", "à", "á", "â", "ã", "ä", "å", "ç", "è", "é", "ê", "ë", "ì", "í", "î", "ï", "ñ", "ò", "ó", "ô", "õ", "ö", "ù", "ú", "û", "ü", "ý", "ÿ", "Ā", "ā", "Ă", "ă", "Ą", "ą", "Ć", "ć", "Ĉ", "ĉ", "Ċ", "ċ", "Č", "č", "Ď", "ď", "Đ", "đ", "Ē", "ē", "Ĕ", "ĕ", "Ė", "ė", "Ę", "ę", "Ě", "ě", "Ĝ", "ĝ", "Ğ", "ğ", "Ġ", "ġ", "Ģ", "ģ", "Ĥ", "ĥ", "Ħ", "ħ", "Ĩ", "ĩ", "Ī", "ī", "Ĭ", "ĭ", "Į", "į", "İ", "ı", "Ķ", "ķ", "ĸ", "Ĺ", "ĺ", "Ļ", "ļ", "Ľ", "ľ", "Ŀ", "ŀ", "Ł", "ł", "Ń", "ń", "Ņ", "ņ", "Ň", "ň", "ŉ", "Ŋ", "ŋ", "Ō", "ō", "Ŏ", "ŏ", "Ő", "ő", "Ŕ", "ŕ", "Ŗ", "ŗ", "Ř", "ř", "Ś", "ś", "Ŝ", "ŝ", "Ş", "ş", "Š", "š", "Ţ", "ţ", "Ť", "ť", "Ŧ", "ŧ", "Ũ", "ũ", "Ū", "ū", "Ŭ", "ŭ", "Ů", "ů", "Ű", "ű", "Ų", "ų", "Ŵ", "ŵ", "Ŷ", "ŷ", "Ÿ", "Ź", "ź", "Ż", "ż", "Ž", "ž", "ſ"};
-         String[] nondiacritics = new String[]{"A", "A", "A", "A", "A", "A", "C", "E", "E", "E", "E", "I", "I", "I", "I", "N", "O", "O", "O", "O", "O", "U", "U", "U", "U", "Y", "s", "a", "a", "a", "a", "a", "a", "c", "e", "e", "e", "e", "i", "i", "i", "i", "n", "o", "o", "o", "o", "o", "u", "u", "u", "u", "y", "y", "A", "a", "A", "a", "A", "a", "C", "c", "C", "c", "C", "c", "C", "c", "D", "d", "D", "d", "E", "e", "E", "e", "E", "e", "E", "e", "E", "e", "G", "g", "G", "g", "G", "g", "G", "g", "H", "h", "H", "h", "I", "i", "I", "i", "I", "i", "I", "i", "I", "i", "K", "k", "k", "L", "l", "L", "l", "L", "l", "L", "l", "L", "l", "N", "n", "N", "n", "N", "n", "N", "n", "N", "O", "o", "O", "o", "O", "o", "R", "r", "R", "r", "R", "r", "S", "s", "S", "s", "S", "s", "S", "s", "T", "t", "T", "t", "T", "t", "U", "u", "U", "u", "U", "u", "U", "u", "U", "u", "U", "u", "W", "w", "Y", "y", "Y", "Z", "z", "Z", "z", "Z", "z", "s"};
-         for (int i = 0; i < diacritics.length; i++) {
-         if (this.texttocipher.contains(diacritics[i])) {
-         this.texttocipher = this.texttocipher.replace(diacritics[i], nondiacritics[i]);
-         }
-         }*/
-        String normalized = java.text.Normalizer.normalize(this.texttocipher, java.text.Normalizer.Form.NFD);// change stanart czech diacritic to non diacritic
-        this.texttocipher = normalized.replaceAll("\\p{InCombiningDiacriticalMarks}+", "");// remove diacritical marks which left after normalizer
-        return this.texttocipher;
-    }
-
-    /**
-     * tato metoda převede text na velká písmena (nepočítá již s diakritikou)
-     *
-     * @return vrátí String texttocipher převedený na velká písmena
-     */
-    public String textToUpperCases() {
-        this.texttocipher = this.texttocipher.toUpperCase();
-        return this.texttocipher;
-    }
+    
 
     @Override
     public void cipher(String string) {
@@ -116,7 +61,7 @@ public class VigenereCipher extends CipherAlgorithm implements Serializable {
             throw new IllegalArgumentException("Text, který se má šifrovat, je kratší než tři znaky");
         }
         this.texttocipher = string;
-        this.key=key;
+        this.key = key;
         this.encipher(key);
         try {
             this.putDataIntoFile();
@@ -127,7 +72,7 @@ public class VigenereCipher extends CipherAlgorithm implements Serializable {
 
     public String encipher() {
         StringBuilder builder = new StringBuilder();
-        String s = this.prepareText();
+        String s = Sifrovani.Helper.adjustStringToLettersAndUpperCases(this.texttocipher);
         generateKey();
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) < 65 || s.charAt(i) > 90) { //znak v ASCII
@@ -149,9 +94,9 @@ public class VigenereCipher extends CipherAlgorithm implements Serializable {
 
     public String encipher(String key) {
         StringBuilder builder = new StringBuilder();
-        String s = this.prepareText();
-        key=prepareKey(key);
-this.key=key;
+        String s = Sifrovani.Helper.adjustStringToLettersAndUpperCases(this.texttocipher);
+        key = Sifrovani.Helper.adjustStringToLettersAndUpperCases(key);
+        this.key = key;
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) < 65 || s.charAt(i) > 90) { //znak v ASCII
                 throw new IllegalArgumentException("Sifrovany retezec neobsahuje jen velka pismena");
@@ -174,13 +119,13 @@ this.key=key;
     private int setLengthOfKey() {
         Random rand = new Random();
         int lenghtofkey = 0;
-        if (this.prepareText().length() < 3) {
+        if (Sifrovani.Helper.adjustStringToLettersAndUpperCases(this.texttocipher).length() < 3) {
             throw new IllegalArgumentException("Text, který se má šifrovat, je kratší než tři znaky");
         }
-        if (this.prepareText().length() > 5) {
-            lenghtofkey = rand.nextInt(2 * this.prepareText().length() / 3 - 2) + 3;
+        if (Sifrovani.Helper.adjustStringToLettersAndUpperCases(this.texttocipher).length() > 5) {
+            lenghtofkey = rand.nextInt(2 * Sifrovani.Helper.adjustStringToLettersAndUpperCases(this.texttocipher).length() / 3 - 2) + 3;
         } else {
-            lenghtofkey = rand.nextInt(this.prepareText().length() - 2) + 3;
+            lenghtofkey = rand.nextInt(Sifrovani.Helper.adjustStringToLettersAndUpperCases(this.texttocipher).length() - 2) + 3;
         }
         return lenghtofkey;
     }
@@ -212,12 +157,7 @@ this.key=key;
      * @param key zadaný klíč
      * @return upravený klíč key
      */
-    public String prepareKey(String key) {
-        key = key.replaceAll("[^\\p{L} ]", "").replaceAll("\\s", "").toUpperCase();
-        key = java.text.Normalizer.normalize(key, java.text.Normalizer.Form.NFD);
-        key = key.replaceAll("\\p{InCombiningDiacriticalMarks}+", "");
-        return  key;
-    }
+    
 
     public String getKey() {
         return this.key;
