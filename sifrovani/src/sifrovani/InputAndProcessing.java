27d1748c48b74ceecdc83bcbd9611f54b16deb74
RadimP <RadimP@192.168.0.213>
2018-02-26 09:53:42 +0100

    Založena třída pro čtení a zpracování vstupu InputAndProcessing a třída Enigma (zatím prázdná, nutno rozpracovat)

---
+++ sifrovani/src/sifrovani/InputAndProcessing.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sifrovani;
+
+/**
+ *
+ * @author RadimP
+ */
+public class InputAndProcessing {
+CaesarCipher caesar;
+Enigma enigma;
+VigenereCipher vigenere;
+SHA1 sha1;
+RSACipher rsa;
+    
+}
