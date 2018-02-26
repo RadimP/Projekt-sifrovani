da5957115c92e36eee65f78063418c6be8ab94ac
RadimP <RadimP@192.168.0.213>
2018-02-26 11:58:13 +0100

    rozhraní CipherAbstractparent přejmenováno na CipherInterface

---
+++ sifrovani/src/sifrovani/CipherInterface.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sifrovani;
+
+import java.io.File;
+
+/**
+ *
+ * @author Student
+ */
+public interface CipherInterface {
+void cipher(String string);
+void cipher(File file);
+void decipher(String string);
+void decipher(File file);
+}
